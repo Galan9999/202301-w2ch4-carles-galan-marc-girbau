0147d9b28a823c5e43587a62ad4766b68488b9db
Marc Girbau <marc.girbau94@gmail.com>
2023-01-20 08:11:29 +0100

    Add console logs to tests

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,37 @@
 import CodersArray from "./CodersArray/CodersArray.js";
 
-const example = new CodersArray(2, 3, 4, 5, 6);
+const firstName = "Carles";
+const secondName = "Marc";
+const thirdName = "Inge";
+const fourthName = "Maxi";
 
-console.log(example);
+const expectedObject = {
+  0: "Carles",
+  1: "Marc",
+  2: "Inge",
+  3: "Maxi",
+  length: 4,
+};
+
+const object = new CodersArray(firstName, secondName, thirdName, fourthName);
+
+console.log(`Expected output:`);
+console.log(expectedObject);
+console.log("Output:");
+console.log(object);
+console.log("Property length test passed");
+
+const otherName = "Carles";
+const anotherdName = "Marc";
+
+const expectedLength = 4;
+
+const namesList = new CodersArray("Inge", "Maxi");
+
+const listLength = namesList.push(otherName, anotherdName);
+
+console.log(`Expected output:`);
+console.log(expectedLength);
+console.log("Output:");
+console.log(listLength);
+console.log("Method push test passed");
